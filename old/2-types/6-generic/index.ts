33c7cebdeb3979bbc535b27bf26d6e81736cd5e1
Igor Nepipenko <inepipenko@muellerone.com>
2022-01-23 13:27:32 +0200

    feat(no-ref): conditional

---
+++ old/2-types/6-generic/index.ts
@@ -0,0 +1,79 @@
+import '../assets/css/style.css';
+
+// function sum(_a: number, _b: number) {
+// 	// ....
+// }
+//
+// sum(1, 2);
+// sum(3, 4);
+
+// interface/type/function/class
+
+// interface IAccount<id, info extends { male: boolean } = { male: boolean }> {
+// 	id: id;
+// 	name: string;
+// 	info: info;
+// }
+//
+// // interface IAdmin {
+// // 	id: number;
+// // 	name: string;
+// // }
+//
+// let user: IAccount<string> = {
+// 	id: 'asdasd123123',
+// 	name: 'Ihor',
+// 	info: {
+// 		male: true,
+// 	},
+// };
+//
+// let admin: IAccount<number, { male: boolean; subject: string[] }> = {
+// 	id: 12313,
+// 	name: 'Elena',
+// 	info: {
+// 		male: false,
+// 		subject: ['ts', 'js'],
+// 	},
+// };
+//
+// [1, 2, 3, 4].reduce((str: string, item: number) => {
+// 	return `${str},${item.toString()}`;
+// }, '');
+
+interface IUser {
+	name: string;
+	age: number;
+}
+
+interface IProduct {
+	name: string;
+	price: number;
+}
+
+interface IState {
+	user: IUser;
+	products: IProduct[];
+	bonuses: number;
+}
+
+type Select<T> = <F extends keyof T>(state: T, field: F) => T[F];
+
+const state: IState = {
+	user: { name: 'Ihor', age: 36 },
+	products: [
+		{ name: 'IPhone X', price: 50 },
+		{ name: 'IPad', price: 30 },
+	],
+	bonuses: 0.8,
+};
+
+const select: Select<IState> = (storeState, fields) => storeState[fields];
+
+const user: IUser = select(state, 'user');
+const products: IProduct[] = select(state, 'products');
+const bonuses: number = select(state, 'bonuses');
+
+// type date = `${''}${number}.${number}${number}.${number}${number}`;
+//
+// let d: date = '12.22.222222';
--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,111 @@
 import '../assets/css/style.css';
 
-// function sum(_a: number, _b: number) {
-// 	// ....
-// }
-//
-// sum(1, 2);
-// sum(3, 4);
+// T extends U ? X : Y;
+// string | never | number =>  string | number;
+type snbun = string | undefined | number | null | boolean;
 
-// interface/type/function/class
-
-// interface IAccount<id, info extends { male: boolean } = { male: boolean }> {
-// 	id: id;
-// 	name: string;
-// 	info: info;
-// }
-//
-// // interface IAdmin {
-// // 	id: number;
-// // 	name: string;
-// // }
-//
-// let user: IAccount<string> = {
-// 	id: 'asdasd123123',
-// 	name: 'Ihor',
-// 	info: {
-// 		male: true,
-// 	},
-// };
-//
-// let admin: IAccount<number, { male: boolean; subject: string[] }> = {
-// 	id: 12313,
-// 	name: 'Elena',
-// 	info: {
-// 		male: false,
-// 		subject: ['ts', 'js'],
-// 	},
-// };
-//
-// [1, 2, 3, 4].reduce((str: string, item: number) => {
-// 	return `${str},${item.toString()}`;
-// }, '');
-
-interface IUser {
+interface IHydrantA {
 	name: string;
-	age: number;
+	type: 'A';
 }
 
-interface IProduct {
+interface IHydrantB {
 	name: string;
-	price: number;
+	type: 'B';
 }
 
-interface IState {
-	user: IUser;
-	products: IProduct[];
-	bonuses: number;
+interface IHydrantC {
+	name: string;
+	type: 'C';
 }
 
-type Select<T> = <F extends keyof T>(state: T, field: F) => T[F];
-
-const state: IState = {
-	user: { name: 'Ihor', age: 36 },
-	products: [
-		{ name: 'IPhone X', price: 50 },
-		{ name: 'IPad', price: 30 },
-	],
-	bonuses: 0.8,
+let hydrant: IHydrantC = {
+	name: 'C',
+	type: 'C',
 };
 
-const select: Select<IState> = (storeState, fields) => storeState[fields];
+type Hydrant = IHydrantA | IHydrantB | IHydrantC;
 
-const user: IUser = select(state, 'user');
-const products: IProduct[] = select(state, 'products');
-const bonuses: number = select(state, 'bonuses');
+function metricAB(h: Exclude<Hydrant, IHydrantC>) {}
+
+function metricC(h: Exclude<Hydrant, Exclude<Hydrant, IHydrantC>>) {}
+
+function metricCB(h: Exclude<Hydrant, IHydrantA>) {}
+
+// let v: {
+// 	name: string;
+// 	type: 'A';
+// } | null;
+//
+// type nonUndefined<T> = T extends undefined | string | null ? never : T;
+// const v1: nonUndefined<snbun> = undefined; //  never | never | number | never | boolean
+//
+// let d: nonUndefined<typeof v> = null;
+//
+// type ReturnValueType<T extends (...args: any) => any> = T extends (...args: unknown[]) => infer R
+// 	? R
+// 	: never;
+//
+// let cb = () => {
+// 	return hydrant;
+// };
+//
+// let v: ReturnValueType<typeof cb> = 1;
+
+// type cbs = [() => number | string, () => boolean, () => number];
+// type cbs1 = [() => Hydrant, () => boolean, () => number];
+//
+// type FirstTupleElReturnType<Tuple> = Tuple extends [infer Cb, ...unknown[]]
+// 	? Cb extends (...args: unknown[]) => infer ReturnValue
+// 		? ReturnValue
+// 		: never
+// 	: never;
+//
+// let v: FirstTupleElReturnType<cbs1> = 1;
+
+// type FnUnionFromParamsAndReturnValue<T extends Function> = T extends (
+// 	...args: [infer A, infer B]
+// ) => infer R
+// 	? A | B | R
+// 	: never;
+
+// keyof [] ====> 0, 1, 2, pop, reduce, ........
+
+// type OnlyNumKeys<T> = T extends number ? T : never;
+// type NonFunction<T> = T extends Function ? never : T;
+// type FnUnionFromParamsAndReturnValue<T extends Function> = T extends (
+// 	...args: infer Args
+// ) => infer R
+// 	? Args[number] | R
+// 	: // ? Args[OnlyNumKeys<keyof Args>] | R
+// 	  // NonFunction<Args[Exclude<keyof Args, 'length'>]> | R
+// 	  never;
+//
+// let sum = (a: number, b: string) => {
+// 	return a + b;
+// };
+//
+// let cb = (_str: string, _h: IHydrantC, _cb: () => void) => {
+// 	return true;
+// };
+//
+// let v: FnUnionFromParamsAndReturnValue<typeof cb> = 1;
+
+type TrimLeft<T extends string> = T extends `  ${infer Rest}` ? TrimLeft<Rest> : T;
+
+let v: TrimLeft<'     http://localhost' | '   https://localhost'> = '    http://localhost';
+
+type Protocol<S extends string> = S extends `${infer Chars}://${string}` ? Chars : never;
+
+const domain = 'https://localhost';
+let protocol: Protocol<typeof domain> = 'http';
+
+type FlattenType<T> = T extends (infer U)[] ? FlattenType<U> : T;
+
+function deepFlatten<Z extends readonly unknown[]>(_x: Z): FlattenType<Z>[] {
+	throw new Error('');
+}
+
+const arr1: number[] = deepFlatten([1, 2, 3]);
+const arr2: number[] = deepFlatten([1, [1, 2], 3]);
+const arr3: number[] = deepFlatten([1, [1, [1, 2, 3]], 3]);
